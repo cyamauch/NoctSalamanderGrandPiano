462440d21f302b619d7f2cf0a1863e742d52fa56
cyamauch <cyamauch@ir.isas.jaxa.jp>
2024-03-21 13:28:56 +0900

    Adjusted vol_factor of layer[1,2].

--- src/midi_test/mk_scale.sh
+++ src/midi_test/mk_scale.sh
@@ -3,13 +3,13 @@
 if [ "$3" = "" ]; then
   echo "[USAGE]"
   echo "$1 tempo start end"
-  echo "$1 50000 21 108 (ALL)"
-  echo "$1 50000 24 48 (C1 to C3)"
-  echo "$1 50000 36 60 (C2 to C4)"
-  echo "$1 50000 48 72 (C3 to C5)"
-  echo "$1 50000 60 84 (C4 to C6)"
-  echo "$1 50000 72 96 (C5 to C7)"
-  echo "$1 50000 84 108 (C6 to C8)"
+  echo "$1 55000 21 108 (ALL)"
+  echo "$1 55000 24 48 (C1 to C3)"
+  echo "$1 55000 36 60 (C2 to C4)"
+  echo "$1 55000 48 72 (C3 to C5)"
+  echo "$1 55000 60 84 (C4 to C6)"
+  echo "$1 55000 72 96 (C5 to C7)"
+  echo "$1 55000 84 108 (C6 to C8)"
   exit
 fi
 
@@ -21,7 +21,7 @@ KEY_E=$3
 #     v1       v4          v8          v12            v16
 LIST="13 29 35 40 44 49 54 61 68 76 84 92 100 108 116 124"
 
-#TEMPO=50000
+#TEMPO=55000
 #TEMPO=100000
 
 N=1
