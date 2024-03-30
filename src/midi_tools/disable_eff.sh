106ccfbc851bce1334ad5d148a15f0124284784a
cyamauch <cyamauch@ir.isas.jaxa.jp>
2024-03-30 14:02:23 +0900

    Updated midi_tools/disable_eff.sh: to erase CC exept CC64 and CC67.

--- src/midi_tools/disable_eff.sh
+++ src/midi_tools/disable_eff.sh
@@ -16,14 +16,29 @@ while [ "$1" != "" ]; do
 
   $MF2T $1 | tr -d '\r' > _tmp_input.txt
 
-  cat _tmp_input.txt | tr -d '\r' | awk '{ \
+  cat _tmp_input.txt | awk '{ \
     if ( $4 == "\"<Master" && ($5 == "Reverve>\"" || $5 == "Volume>\"") ) { FLG=1; } \
     else { \
       if ( FLG == 1 ) { \
         if ( $2 == "SysEx" ) {FLG=1;} \
         else {FLG=0; print;} \
       } \
-      else {print;} \
+      else { \
+        if ( $2 == "Par" && $4 == "c=64" && substr($5,1,2) == "v=" ) { \
+          V=int(substr($5,3)); \
+          if ( V < 64 ) { printf("%s %s %s %s v=0\n",$1,$2,$3,$4); } \
+          else { printf("%s %s %s %s v=127\n",$1,$2,$3,$4); } \
+        } \
+        else { \
+          if ( $2 == "Par" && substr($4,1,2) == "c=" ) { \
+            if ( $4 == "c=67" ) { print; } \
+          } \
+          else { \
+            if ( ($2 == "On" && $5 == "v=0") && 0 ) { printf("%s Off %s %s %s\n",$1,$3,$4,$5); } \
+            else { print; } \
+          } \
+        } \
+      } \
     } \
   }' > _tmp_output.txt
 
