12eeb1058986657378c8f8ac754a8128faa642a8
cyamauch <cyamauch@ir.isas.jaxa.jp>
2024-04-27 14:49:01 +0900

    Improved volume homogeneity/continuity drastically. Set the reference volume level of each layer: v16,0dB v15,-1.5dB v14,-3dB v13,-4.5dB v12,-6dB v11,-7.5dB v10,-9dB v09,-10.5dB v08,-12dB v07,-13.5dB v06,-15dB v05,-16.5dB v04,-18dB v03,-20dB v02,-23dB v01,-27dB.

--- src/midi_test/boost_scale_test.sh
+++ src/midi_test/boost_scale_test.sh
@@ -3,7 +3,7 @@
 FFMPEG="/cygdrive/c/archives/Piano/VirtualMIDISynth/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe"
 
 
-LIST="v16,+0dB v15,+1dB v14,+2.5dB v13,+4dB v12,+6dB v11,+7.5dB v10,+9.5dB v09,+11.5dB v08,+13dB v07,+14dB v06,+15.5dB v05,+17dB v04,+18dB v03,+20dB v02,+23dB v01,+27dB"
+LIST="v16,+0dB v15,+1.5dB v14,+3dB v13,+4.5dB v12,+6dB v11,+7.5dB v10,+9dB v09,+10.5dB v08,+12dB v07,+13.5dB v06,+15dB v05,+16.5dB v04,+18dB v03,+20dB v02,+23dB v01,+27dB"
 
 for i in $LIST; do
   NAME=`echo $i | sed -e 's/[,][^,]*//'`
