4d2d157417db0913bf55a3091525718da2981f89
cyamauch <cyamauch@ir.isas.jaxa.jp>
2024-05-29 17:35:44 +0900

    Updated param_work/* for Version 5.0.

--- src/param_work/add_mul.sh
+++ src/param_work/add_mul.sh
@@ -1,11 +1,46 @@
+
 #!/bin/sh
 
 
-ARG_TXT_0=`cat $1 | tr -d '\r'`
-ARG_TXT_1=`cat $2 | tr -d '\r'`
+ARG_TXT_0=`cat $1 | tr -d '\r' | sed -e 's/^[ ]*//'`
+ARG_TXT_1=`cat $2 | tr -d '\r' | sed -e 's/^[ ]*//'`
 
-echo "$ARG_TXT_0" | grep '^[0-1]' | sort > _tmp0_.txt
-echo "$ARG_TXT_1" | grep '^[0-1]' | sort > _tmp1_.txt
+echo "$ARG_TXT_0" | grep '^[0-1][0-9][0-9]' | sort > _tmp0_.txt
+echo "$ARG_TXT_1" | awk '{ \
+  if ( NR==1 ) { \
+    VEL_ALL[1] = ""; \
+  } \
+  p0 = match($1, /^VEL_ALL/); \
+  if ( 0 < p0 ) { \
+    split($0,VEL_ALL," "); \
+  } \
+  p0 = match($1, /^[0-1][0-9][0-9]_[A-Z]/); \
+  if ( 0 < p0 ) { \
+    split($0,ARR," "); \
+    if ( ARR[3] == "" ) { \
+      for ( i=2 ; i <= 16 ; i++ ) { \
+        ARR[1+i] = ARR[1+1]; \
+      } \
+    } \
+    if ( VEL_ALL[1] != "" ) { \
+      for ( i=1 ; i <= 16 ; i++ ) { \
+        ARR[1+i] = VEL_ALL[1+i] + ARR[1+i]; \
+      } \
+    } \
+    printf("%s",$1); \
+    for ( i=1 ; i <= 16 ; i++ ) { \
+      printf(" %.2f",ARR[1+i]); \
+    } \
+    printf("\n"); \
+  } \
+}' | sort > _tmp1_.txt
 
-paste _tmp0_.txt _tmp1_.txt | tr '\t' ' ' | awk '{ split($0,ARR," "); printf("%-7s ",ARR[1]); for (i=0 ; i<16 ; i++) { printf("%+.1f ",ARR[2+i]+ARR[19+i]); } printf("\n"); }'
+paste _tmp0_.txt _tmp1_.txt | tr '\t' ' ' | awk '{ \
+  split($0,ARR," "); \
+  printf("%-7s",ARR[1]); \
+  for (i=0 ; i<16 ; i++) { \
+    printf(" %+.2f",ARR[2+i]+ARR[19+i]); \
+  } \
+  printf("\n"); \
+}'
 
--- src/param_work/mk_vol_factor.src4build.sh
+++ src/param_work/mk_vol_factor.src4build.sh
@@ -10,17 +10,13 @@ if [ "$1" = "" ]; then
   exit
 fi
 
-VOL_FACTOR_BASE=`cat vol_factor_base.txt | tr -d '\r'`
-VOL_FACTOR_SRC=`cat $1 | tr -d '\r'`
-##############VOL_FACTOR_EFFECTIVE=`cat vol_factor_effective.txt | tr -d '\r'`
+VOL_FACTOR_BASE=`cat vol_factor_base.txt | tr -d '\r' | sed -e 's/^[ ]*//'`
+VOL_FACTOR_SRC=`cat $1 | tr -d '\r' | sed -e 's/^[ ]*//'`
+VOL_FACTOR_EFFECTIVE=`cat vol_factor_effective.txt | tr -d '\r' | sed -e 's/^[ ]*//'`
 
 LIST=`echo "$VOL_FACTOR_BASE" | grep '^[ACDF]' | awk '{printf("%s,%s\n",$1,$2);}'`
 OFFSET_SRC=`echo "$VOL_FACTOR_BASE" | grep '^OFFSET_SRC' | awk '{printf("%s\n",$2);}'`
 
-#echo $LIST
-
-#echo "$VOL_FACTOR_SRC" | grep '^#'
-#echo "$VOL_FACTOR_BASE" | grep '^OFFSET '
 
 echo "#Key Volume(db)"
 echo "#         v1   v2   v3   v4   v5   v6   v7   v8   v9  v10  v11  v12  v13  v14  v15  v16"
@@ -32,21 +28,34 @@ echo ""
 if [ "$VOL_FACTOR_EFFECTIVE" != "" ]; then
 
   echo "$VOL_FACTOR_EFFECTIVE" | grep '^EFF_RATIO' > _tmp0_.txt
-  echo "$VOL_FACTOR_EFFECTIVE" | grep '^[0-1]' | sort >> _tmp0_.txt
+  echo "$VOL_FACTOR_EFFECTIVE" | grep '^[0-1][0-9][0-9]' | sort >> _tmp0_.txt
 
   echo "" > _tmp1_.txt
-  echo "$VOL_FACTOR_SRC" | grep '^[0-1]' | sort >> _tmp1_.txt
+  echo "$VOL_FACTOR_SRC" | grep '^[0-1][0-9][0-9]' | sort >> _tmp1_.txt
 
-  paste _tmp0_.txt _tmp1_.txt | tr '\t' ' ' | awk '{ if(NR==1){V1=$2; V2=$3; V3=$4; V4=$5; V5=$6; V6=$7; V7=$8; V8=$9; V9=$10; V10=$11; V11=$12; V12=$13; V13=$14; V14=$15; V15=$16; V16=$17;} else { printf("%-7s %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f\n",$1,$4+V1*$2,$5+V2*$2,$6+V3*$2,$7+V4*$2,$8+V5*$2,$9+V6*$2,$10+V7*$2,$11+V8*$2,$12+V9*$2,$13+V10*$2,$14+V11*$2,$15+V12*$2,$16+V13*$2,$17+V14*$2,$18+V15*$2,$19+V16*$2); } }' > _tmp2_.txt
+  paste _tmp0_.txt _tmp1_.txt | tr '\t' ' ' | awk '{ \
+    if ( NR==1 ) { \
+      split($0,EFF_RATIO," "); \
+    } \
+    else { \
+      split($0,ARR," "); \
+      printf("%s",$1); \
+      for ( i=1 ; i <= 16 ; i++ ) { \
+        printf(" %+.2f",ARR[3+i] + EFF_RATIO[1+i] * ARR[1+1]); \
+      } \
+      printf("\n"); \
+    } \
+  }' > _tmp2_.txt
+
 
 else
 
-  echo "$VOL_FACTOR_SRC" | grep '^[0-1]' | sort > _tmp2_.txt
+  echo "$VOL_FACTOR_SRC" | grep '^[0-1][0-9][0-9]' | sort > _tmp2_.txt
 
 fi
 
 
-echo "$VOL_FACTOR_BASE" | grep '^[0-1]' | awk '{printf("%s %g\n",$1,'$OFFSET_SRC'+$2);}' | sort > _tmp0_.txt
+echo "$VOL_FACTOR_BASE" | grep '^[0-1][0-9][0-9]' | awk '{printf("%s %g\n",$1,'$OFFSET_SRC'+$2);}' | sort > _tmp0_.txt
 
 VEL_LINE=`echo "$VOL_FACTOR_SRC" | grep '^VEL'`
 if [ "$VEL_LINE" != "" ]; then
@@ -56,7 +65,26 @@ else
 fi
 
 
-paste _tmp0_.txt _tmp2_.txt | tr '\t' ' ' | awk '{printf("%-7s %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f\n",$3,$4+$2,$5+$2,$6+$2,$7+$2,$8+$2,$9+$2,$10+$2,$11+$2,$12+$2,$13+$2,$14+$2,$15+$2,$16+$2,$17+$2,$18+$2,$19+$2);}' >> _tmp3_.txt
+paste _tmp0_.txt _tmp2_.txt | tr '\t' ' ' | awk '{ \
+  split($0,ARR," "); \
+  printf("%s",$3); \
+  for ( i=1 ; i <= 16 ; i++ ) { \
+    printf(" %+.2f",ARR[3+i] + ARR[1+1]); \
+  } \
+  printf("\n"); \
+}' >> _tmp3_.txt
 
-cat _tmp3_.txt | awk '{ if(NR==1){V1=$2; V2=$3; V3=$4; V4=$5; V5=$6; V6=$7; V7=$8; V8=$9; V9=$10; V10=$11; V11=$12; V12=$13; V13=$14; V14=$15; V15=$16; V16=$17;} else { printf("%-7s %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f %+.1f\n",$1,V1+$2,V2+$3,V3+$4,V4+$5,V5+$6,V6+$7,V7+$8,V8+$9,V9+$10,V10+$11,V11+$12,V12+$13,V13+$14,V14+$15,V15+$16,V16+$17); } }'
+cat _tmp3_.txt | awk '{ \
+  if ( NR==1 ) { \
+    split($0,VEL_ALL," "); \
+  } \
+  else { \
+    split($0,ARR," "); \
+    printf("%-7s",$1); \
+    for ( i=1 ; i <= 16 ; i++ ) { \
+      printf(" %+.1f",VEL_ALL[1+i] + ARR[1+i]); \
+    } \
+    printf("\n"); \
+  } \
+}'
 
