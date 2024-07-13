67a4b123a3ba0f2bf381ba263e7dc92c19d09b97
cyamauch <cyamauch@ir.isas.jaxa.jp>
2024-07-13 22:02:45 +0900

    Fixed small bug of volumne_measurement/measure_all_volumes.sh.

--- src/volumne_measurement/measure_all_volumes.sh
+++ src/volumne_measurement/measure_all_volumes.sh
@@ -13,9 +13,12 @@ DIR=../../AccurateSalamanderGrandPianoV5.0_48khz24bit/48khz24bit/
 
 PLOT_CMD="plot "
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 ; do
+LAYER_LIST="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16"
+#LAYER_LIST="02  06  08  10  14"
 
-  echo "Measuring velocity:${i}"
+for i in $LAYER_LIST ; do
+
+  echo "Measuring velocity: ${i}"
 
   LIST=`ls $DIR | grep '[0-1][0-9][0-9]_[^_v]*'"v${i}"'[.]wav' | awk '{printf("%s/%s\n","'${DIR}'",$1);}'`
 
@@ -30,5 +33,5 @@ for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 ; do
 
 done
 
-echo $PLOT_CMD > plot_meanvol-sec${SEC}_v${i}.txt
+echo $PLOT_CMD > plot_meanvol-sec${SEC}.txt
 
