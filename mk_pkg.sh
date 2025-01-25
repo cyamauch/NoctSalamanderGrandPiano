5c806e554d6d60a3f92ec65a3dde8b40dedebfb4
cyamauch <cyamauch@ir.isas.jaxa.jp>
2025-01-25 16:49:38 +0900

    Updated mk_pkg.sh.

--- mk_pkg.sh
+++ mk_pkg.sh
@@ -3,14 +3,25 @@
 if [ "$1" = "" ]; then
   echo "[USAGE]"
   echo "$0 directory_name"
+  echo "$0 directory_name sfzonly"
   exit
 fi
 
 PKG=$1
+FLG=$2
 
 if [ -d $PKG ]; then
   cd $PKG
-  zip -r ../${PKG}.zip * -x *_test-only*.sfz
+  if [ "$FLG" != "" ]; then
+    if [ "$FLG" = "sfzonly" ]; then
+      zip -r ../${PKG}.sfz-only.zip *.sfz src -x *_test-only*.sfz
+    else
+      echo "Invalid arg: $FLG"
+      exit 127
+    fi
+  else
+    zip -r ../${PKG}.zip * -x *_test-only*.sfz
+  fi
   cd ..
 fi
 
